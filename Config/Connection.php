bc562ca8e66600992311677944cd230b552f4490
Ahmad Rusyadi <gerdasofya@outlook.co.id>
2021-11-25 10:48:49 +0800

    Update Connection.php

--- Config/Connection.php
+++ Config/Connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "bqcvozq86rgzrsx3milt-mysql.services.clever-cloud.com";
+$username = "ufmtfy1i6xcomm8i";
+$password = "SGi7zHccgeiu5l5MkaAh";
 $koneksi;
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=dcampdb", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=bqcvozq86rgzrsx3milt", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
