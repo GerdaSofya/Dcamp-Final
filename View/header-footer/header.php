2250b972bfe26d45ab5898b734733265b12a3797
Afrizal2609 <afrizal.bbm123@gmail.com>
2021-12-10 09:51:18 +0700

    Fix Bug Header

--- View/header-footer/header.php
+++ View/header-footer/header.php
@@ -4,7 +4,7 @@
     <div class="header-logo">DCamp</div>
     <div class="header-list">
         <ul>
-            <li><a href="AboutUsUser.php">About Us</a></li>
+            <li><a href="View/AboutUsBeranda.php">About Us</a></li>
             <li><a href="View/login.php">Login</a> </li>
             <li><a href="index.php">Beranda</a> </li>
         </ul>
